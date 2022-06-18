be56034f965a2ded4cb99acbd119b86d711b6a7a
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-06-18 12:18:05 +0300

    deleted index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const {start} = require('./src/server');
-// const fs = require("fs").promises;
-// const path = require("node:path");
-start();
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors'); // кросдоменні запроси
+require('dotenv').config();
+const cors = require('cors'); // кросдоменні запроси
 const morgan = require('morgan'); // logger
-const {connectMongo} = require('./db/connection');
-const {router} = require('./routers/postsRouter');
+const {connectMongo} = require('./src/db/connection');
+const {router} = require('./src/routers/postsRouter');
 
 const PORT = process.env.PORT || 8090;
 
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use('/api/posts', router);
-
+app.use(cors);
 app.use((error, req, res, next) => {
   res.status(500).json({message: error.message});
 });
@@ -30,6 +31,4 @@ const start = async () => {
   }
 };
 
-module.exports = {
-  start,
-};
+start();
